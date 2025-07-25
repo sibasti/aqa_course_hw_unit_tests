b6f63aeba20f48d19a670c07f1ed9ad973490333
Denis <kenpaticroissant@gmail.com>
2025-07-25 11:52:38 +0300

    new homework

---
+++ firstHomeWork.js
@@ -0,0 +1,37 @@
+//TASK1
+let item_1 = 5;
+console.log (item_1);
+
+let item_2 = 3;
+console.log (item_2);
+
+let item_3 = item_1 + item_2;
+console.log(item_3);
+
+let item_4 = 'Hello World';
+console.log(item_4);
+
+console.log(item_3+item_4);
+console.log(item_3*item_4);
+
+let item_5 = item_3;
+
+let item_6 = 15;
+let item_6_type = typeof item_6;
+
+console.log("item_6 == "+ item_6,"item_6_type == "+ item_6_type);
+
+let item_7 = String(item_6);
+let item_7_type = typeof(item_7);
+
+console.log("item_7 == "+item_7, "item_7_type == "+ item_7_type);
+
+//TASK2 
+
+let n = 4;
+
+let nn = String(n)+String(n);
+let nnn = String(n)+String(n)+String(n);
+
+console.log(Number(n)+Number(nn)+Number(nnn));
+
