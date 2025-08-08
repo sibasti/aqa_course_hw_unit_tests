3afee35faa346abba3cae496f83e87bbcee6c113
sibas <kenpaticroissant@gmail.com>
2025-08-08 16:49:51 +0300

    done

--- homework-solutions/js/hw-6/task-2.mjs
+++ homework-solutions/js/hw-6/task-2.mjs
@@ -13,7 +13,36 @@
   const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
 */
 
-let resultUnique;
-let resultNull;
+const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai']
+const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
+const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+
+function getUniquePizzas(myPizzas, competitorPizzas){
+  let unique = [];
+  
+  for(let i = 0; i < myPizzas.length; i++) {
+    const myPizzaLower = myPizzas[i].toLowerCase();
+
+    let found = false;
+
+    for (let j = 0; j < competitorPizzas.length; j++) {
+      if (myPizzaLower === competitorPizzas[j].toLowerCase()){
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      unique.push(myPizzas[i]);
+    }
+  }
+  return unique.length > 0 ? unique: null;
+}
+
+let resultUnique = getUniquePizzas(myPizzasT1,competitorPizzas);
+
+let resultNull = getUniquePizzas(myPizzasT2,competitorPizzas);
+
+console.log(resultUnique);
+console.log(resultNull);
 
 export { resultNull, resultUnique };
--- homework-solutions/js/hw-6/task-3.mjs
+++ homework-solutions/js/hw-6/task-3.mjs
@@ -6,6 +6,18 @@
 
   Присвойте результат в переменную "unique"
 */
-let unique;
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 4, 6, 8, 10, 1, 3, 5, 7, 9];
+
+for(let i = 0; i < arr.length; i++){
+  for (let j = i+1; j < arr.length; j++){
+    if (arr[i]===arr[j]){
+      arr.splice(j,1);
+      j--;
+    }
+  }
+}
+let unique = arr;
+
+console.log(unique);
 
 export { unique };
